8dc5324e7eb5f0fe5bbe879e6d197f0de68d1d01
Francisco da silva costa <80585387+franciscocosta4@users.noreply.github.com>
2026-03-25 18:56:14 +0000

    backend setup

---
+++ backend/src/main/java/com/example/springexpenses/SpringexpensesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringexpensesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/springexpenses/SpringexpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringexpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
