dc84c2ca78485e2a45bf8c60be3d0a76417c88c0
Francisco da silva costa <80585387+franciscocosta4@users.noreply.github.com>
2026-04-15 20:31:20 +0100

    front page data working

--- backend/src/main/java/com/example/springexpenses/controller/DashboardController.java
+++ backend/src/main/java/com/example/springexpenses/controller/DashboardController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springexpenses.service.IncomeService;
+import com.example.springexpenses.service.ExpenseService;
 import com.example.springexpenses.dto.DashboardDTO;
 
 
@@ -16,6 +17,8 @@ public class DashboardController {
 
     @Autowired
     private IncomeService incomeService;
+    @Autowired
+    private ExpenseService expenseService;
 
     /**
      * Buscar incomes do utilizador autenticado
@@ -27,16 +30,16 @@ public class DashboardController {
 
         // dados vindos dos services
         var totalIncome = incomeService.getUserTotalIncome(email);
-        //! var totalExpenses = expenseService.getUserTotalExpenses(email);
+        var totalExpenses = expenseService.getUserTotalExpense(email);
 
         // cálculo no backend
-        //! var balance = totalIncome.subtract(totalExpenses);
+        var balance = totalIncome.subtract(totalExpenses);
 
         // construir resposta
         DashboardDTO dto = new DashboardDTO();
         dto.setTotalIncome(totalIncome);
-        //! dto.setTotalExpenses(totalExpenses);
-        //! dto.setBalance(balance);
+        dto.setTotalExpenses(totalExpenses);
+        dto.setBalance(balance);
 
         return dto;
     }
